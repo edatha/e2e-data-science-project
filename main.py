ec9083d29d992184734ab71d883a941d6d336cef
Erlangga Dwi Atha <dwiathaerlangga@gmail.com>
2025-03-06 14:52:15 +0700

    Data Validation completed

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from src.datascience import logger
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.datascience.pipeline.data_validation_pipeline import DataIngestionValidationPipeline
 
-logger.info("Welcome to our custom logging data science")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.initiate_data_ingestion()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionValidationPipeline()
+        obj.initiate_data_validation()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
